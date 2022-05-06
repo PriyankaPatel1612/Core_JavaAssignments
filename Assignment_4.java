04d9ca8be5edee0d1d7ad51556e900c9b7246237
Priyanka Patel <priya.priyanka1612@gmail.com>
2022-05-06 11:17:11 +0530

    comments cleared

--- Assignment_4.java
+++ Assignment_4.java
@@ -3,7 +3,7 @@ class Assignment_4
 {
    public static void main(String[] args)
    {
-     //Account obj1 = new Account();
+     
      AccountOverDrawDemo obj2 = new AccountOverDrawDemo();
      Thread one = new Thread(obj2);
         Thread two = new Thread(obj2);
@@ -12,7 +12,7 @@ class Assignment_4
         two.setPriority(10);
         one.start();
         two.start();
-     //AccountOverDrawSafeDemo obj3 = new AccountOverDrawSafeDemo();
+     
     }
 }
 
