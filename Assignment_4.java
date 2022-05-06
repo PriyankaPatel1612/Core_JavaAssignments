f422adab9ccefaf6ab8c6d3adb0fec8359c196bc
Priyanka Patel <priya.priyanka1612@gmail.com>
2022-05-06 11:15:57 +0530

    comments cleared

--- Assignment_4.java
+++ Assignment_4.java
@@ -17,17 +17,3 @@ class Assignment_4
 }
 
 
-/*
-public class Master {
-    public static void main(String[] args) {
-        AccountOverDrawDemo acc = new AccountOverDSrawDemo();
-        Thread one = new Thread(acc);
-        Thread two = new Thread(acc);
-        one.setName("Pratham");
-        two.setName("Prachi");
-        two.setPriority(10);
-        one.start();
-        two.start();
-    }
-}
-*/
